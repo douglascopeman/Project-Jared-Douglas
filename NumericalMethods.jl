d7b5ed2e697f7f6b070bd6f78ad38bd7276f3c12
Douglas Copeman <91123754+douglascopeman@users.noreply.github.com>
2024-09-12 10:47:12 +0000

    Seperated functions into modules to make them easier to read and find

---
+++ NumericalMethods.jl
@@ -0,0 +1,33 @@
+include("modelSpace.jl")
+using .ModelSpace
+
+
+
+module NumericalMethods
+
+export symplecticEuler!
+
+
+"""
+    symplecticEuler!(spaceData::Vector{Body})
+
+The symplectic euler numerical method, calculates the velocity at timestep n+1 using it along with the n position step
+to calculate the position at n+1
+"""
+function symplecticEuler!(spaceData::Vector{Main.ModelSpace.Body})
+    α = accelerationCalc(spaceData)     # Gives acceleration values at timestep n for all bodies and all axes
+    
+    # Updates the entries in each isntance of the class
+    for (i,p) in enumerate(spaceData)
+        p.t += p.dt             # Time update
+        p.dx += p.dt * α[i,1]   # x-velocity 
+        p.dy += p.dt * α[i,2]   # y-velocity
+        p.dz += p.dt * α[i,3]   # z-velocity
+        p.x += p.dt * p.dx      # x-position
+        p.y += p.dt * p.dy      # y-position
+        p.z += p.dt * p.dz      # z-position
+    end
+end
+
+
+end
