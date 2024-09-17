d954fdf604822ec292bd886a040d35248216bca3
Jared Bruggink <s2162590@ed.ac.uk>
2024-09-17 12:39:03 +0100

    added energy calc. To be tested

--- Plotter/Body.py
+++ Plotter/Body.py
@@ -4,15 +4,14 @@ class Body():
     
     def __init__(self, mass, dt, positions, velocities, plot_colour):
         self.mass = mass
-        self.position = positions
-        self.velocity = velocities
-        self.acceleration = np.zeros(3)
+        self.positions = positions
+        self.velocities = velocities
         self.plot_colour = plot_colour
         
     def kinetic_energies(self):
         '''Calculates the kinetic energy of the body at each timestep and returns the result as a numpy array'''
-        kinetic_energy = np.zeros_like(self.velocity)
-        for i, velocity in enumerate(self.velocity):
+        kinetic_energy = np.zeros_like(self.velocities)
+        for i, velocity in enumerate(self.velocities):
             T = np.dot(velocity, velocity) * self.mass / 2
             kinetic_energy[i] = T
         return kinetic_energy
--- Plotter/Plotter.py
+++ Plotter/Plotter.py
@@ -4,6 +4,8 @@ import Body
 
 class Plotter():
     def __init__(self, filename, **plot_kwargs):
+        self.G = 6.67408e-11
+        
         self.filename = filename
         defaultKwargs = {}
         self.plot_kwargs = defaultKwargs | plot_kwargs
@@ -17,10 +19,18 @@ class Plotter():
         
         
     def calculate_total_energy(self):
+        '''Calculates the total energy of the system at each timestep and returns the result as a numpy array'''
+        #TODO: check implimentation works with arrays and tweak if necessary
         T = sum(body.kinetic_energies() for body in self.bodies)
         
         twice_U = 0.0
         for body in self.bodies:
             for other_body in self.bodies:
                 if body is not other_body:
-                    pass #TODO: Implement calculation to determine direction between bodies and then calculate the doubled U
+                    for position in body.positions:
+                        direction = np.linalg.norm(body.position - other_body.position)
+                        twice_U -= self.G * body.mass * \
+                            other_body.mass / direction
+                            
+        U = twice_U / 2
+        return T + U
