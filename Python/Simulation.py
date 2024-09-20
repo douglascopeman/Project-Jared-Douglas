9b5c3a724453d2c65545cc16ae80f740ddd61f30
Jared Bruggink <s2162590@ed.ac.uk>
2024-09-20 12:18:18 +0100

    changed main to the Simulation Class

---
+++ Python/Simulation.py
@@ -0,0 +1,74 @@
+import numpy as np
+from numpy import linalg as LA
+from Body import Body
+import integrators
+from Plotter import Plotter
+
+class Simulation():
+    
+    def __init__(self, T, dt, bodies, G=1):
+        self.G = G
+        self.bodies = bodies
+        self.n = len(bodies)
+        self.T = T
+        self.dt = dt
+
+
+###################################################
+# Run Model
+###################################################
+    def run(self):
+        simulationSettings = np.array([self.T, self.dt, self.n])
+        simulation = self.runTwo(self.bodies)
+        np.savetxt("Outputs\\simulationSettings.csv", simulationSettings, delimiter=",")
+        for i in range(self.n):
+            np.savetxt("Outputs\\output" + str(i) + ".csv", simulation[:,:,i], delimiter=",")
+
+###################################################
+# Simulation Calculations
+###################################################
+
+    def calculateAccelerations(self):
+        """
+        Compute the acceleration between n bodies in the x,y and z axes. The output will be a nx3 array.
+        """
+
+        acceleration = np.zeros((self.n,3), dtype=float)
+        for body in range(0,self.n):
+            acceleration[body,:] = np.sum([
+                ((-self.G * self.bodies[i].mass)/((LA.norm(self.bodies[body].position - self.bodies[i].position))**3))*(self.bodies[body].position - self.bodies[i].position) for i in (set(range(0,self.n)))-set([body])], axis = 0)
+    
+        return acceleration
+
+    def runTwo(self, bodies, Integrator=integrators.symplecticEuler):
+        """
+        Builds a 3 dimensional array filled with the 3 axes position data of every body for the length of the simulaiton
+        """
+        simulation = np.zeros((self.T, 6, self.n))
+        # modelHamiltonian = np.zeros(simLength)
+        for i in range(0, self.T):
+            accelerations = self.calculateAccelerations()
+            bodies = Integrator(bodies, accelerations, self.dt)
+            for p in range(0,self.n):
+                simulation[i,:,p] = np.concatenate((bodies[p].position, bodies[p].velocity), axis=None)
+    
+        return simulation
+###################################################
+# Run Model
+###################################################
+# if __name__ == "__main__":
+#     T = 400    # Number of "frames" in simulation
+#     dt = 1    # Timestep between each frame
+#     n = len(bodies)
+
+#     simulationSettings = np.array([T, dt, n])
+#     simulation = runSimulation(bodies, T, dt)
+#     np.savetxt("Outputs\\simulationSettings.csv", simulationSettings, delimiter=",")
+#     for i in range(len(bodies)):
+#         np.savetxt("Outputs\\output" + str(i) + ".csv", simulation[:,:,i], delimiter=",")
+###################################################
+# Plotting
+###################################################
+# if __name__ == "__main__":
+#     plot = Plotter("Outputs\\")
+
