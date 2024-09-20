bed1f6ecc2128128c854059683b35a034cc3126f
Jared Bruggink <s2162590@ed.ac.uk>
2024-09-20 11:18:19 +0100

    added run file to call main and plotter

--- Python/run.py
+++ Python/run.py
@@ -1,7 +1,7 @@
 import mainPython
 from Body import Body
 import numpy as np
-from Plotter import Plotter
+import Plotter
 
 earth = Body(np.array([0,0,0], dtype=float), np.array([0,1,0], dtype=float), 1000)
 moon =  Body(np.array([0,10,0], dtype=float), np.array([10,0,0], dtype=float), 1)
@@ -10,4 +10,4 @@ T = 1000    # Number of "frames" in simulation
 dt = 1    # Timestep between each frame
 
 mainPython.run(T, dt, bodies)
-plotter = Plotter("Outputs")
+plotter = Plotter.Plotter("Outputs")
