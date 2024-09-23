0bebf1753fccf3623f5a5b53bee29dd3f304fb07
Jared Bruggink <s2162590@ed.ac.uk>
2024-09-23 16:26:36 +0100

    added figure 8 orbit example

--- Python/orbit_examples.py
+++ Python/orbit_examples.py
@@ -11,6 +11,11 @@ class orbit_examples:
     moon =  Body(np.array([5,0,0], dtype=float), np.array([0.5,0,0], dtype=float), 10)
     offset_elliptical = [earth, moon]
     
+    body0 = Body(np.array([0.97000436,-0.24308753,0], dtype=float), np.array([0.46620368,0.43236573,0], dtype=float))
+    body1 = Body(np.array([-0.97000436,0.24308753,0], dtype=float), np.array([0.46620368,0.43236573,0], dtype=float))
+    body2 = Body(np.array([0,0,0], dtype=float), np.array([-0.93240737,-0.86473146,0], dtype=float))
+    figure_eight = [body0, body1, body2]
+    
 if __name__ == "__main__":
     import run
     run.run()
--- Python/run.py
+++ Python/run.py
@@ -4,10 +4,10 @@ import Plotter
 import Simulation
 from orbit_examples import orbit_examples
 
-T = 1000
+T = 10000
 dt = 0.01
 
-simulation = Simulation.Simulation(T, dt, orbit_examples.linear_start_choreography)
+simulation = Simulation.Simulation(T, dt, orbit_examples.figure_eight)
 simulation.run()
-plotter = Plotter.Plotter("Outputs", plot_energy=True)
+plotter = Plotter.Plotter("Outputs", plot_centre_of_mass=True, plot_energy=True)
 plotter.plot()
