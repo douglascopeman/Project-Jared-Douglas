2b4342a2a7f3e81b9e364072d8c243718c38fd25
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-02 11:00:00 +0100

    added java simulation class

--- Java/nbodysimulation/Vector.java
+++ Java/nbodysimulation/Vector.java
@@ -2,12 +2,24 @@ package nbodysimulation;
 
 public class Vector{
 
-    double x, y, z;
+    private final double x, y, z;
 
     public Vector(double x, double y, double z){
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public double getX(){
+        return this.x;
+    }
+
+    public double getY(){
+        return this.y;
+    }
+
+    public double getZ(){
+        return this.z;
     }
 
     public Vector add(Vector other){
