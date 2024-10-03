beeabe28b7b70f4e8f830327d14448a35c4d9287
douglascopeman <s2147499@ed.ac.uk>
2024-10-03 13:08:39 +0100

    Taking a shot at the variable timestep. It works but isn't simplectic. Lots of things to talk about in the meeting tomorrow.

--- Python/run.py
+++ Python/run.py
@@ -6,10 +6,11 @@ from orbit_examples import orbit_examples
 import Integrators
 import time
 
-N = 5000 #rename to N
-dt = 0.01
+N = 10000 #rename to N
+dt = 0.02
 
-simulation = Simulation.Simulation(N, dt, orbit_examples.elliptical)
+simulation = Simulation.Simulation(N, dt, orbit_examples.elliptical, Integrator=Integrators.symplecticEuler, variable_dt_constant = 0.01)
+
 
 # start = time.time()
 simulation.run()
