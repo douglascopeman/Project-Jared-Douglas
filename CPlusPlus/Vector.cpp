80e6f46ddad771ca364459ead089757bb01e7d8b
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-08 16:31:49 +0100

    created header file for vector class

--- CPlusPlus/Vector.cpp
+++ CPlusPlus/Vector.cpp
@@ -1,63 +1,61 @@
 #include <iostream>
 #include <array>
+#include "Vector.h"
 
-class Vector {
-private:
-    std::array<double, 3> elements;
+Vector::Vector() : elements{0.0, 0.0, 0.0} {}
+Vector::Vector(double x, double y, double z) : elements{x, y, z} {}
 
-public:
-    Vector() : elements{0.0, 0.0, 0.0} {}
+//Override the [] subscript operator for the Vector class
+double& Vector::operator[](int index) {
+    return elements[index];
+}
 
-    Vector(double x, double y, double z) : elements{x, y, z} {}
 
-    //Override the [] subscript operator for the Vector class
-    double& operator[](int index) {
-        return elements[index];
+const double& Vector::operator[](int index) const {
+    return elements[index];
+}
+
+//Override the + operator for the Vector class
+Vector Vector::operator+(const Vector& other) const {
+    return Vector(elements[0] + other[0], elements[1] + other[1], elements[2] + other[2]);
+}
+
+//Override the - operator for the Vector class
+Vector Vector::operator-(const Vector& other) const {
+    return Vector(elements[0] - other[0], elements[1] - other[1], elements[2] - other[2]);
+}
+
+double Vector::norm() const {
+    return sqrt(elements[0] * elements[0] + elements[1] * elements[1] + elements[2] * elements[2]);
+}
+
+Vector Vector::scalarMultiply(double scalar) const {
+    return Vector(elements[0] * scalar, elements[1] * scalar, elements[2] * scalar);
+}
+
+Vector Vector::vectorMultiply(const Vector& other) const {
+    return Vector(elements[0] * other[0], elements[1] * other[1], elements[2] * other[2]);
+}
+
+Vector Vector::power(double p) const {
+    return Vector(pow(elements[0], p), pow(elements[1], p), pow(elements[2], p));
+}
+
+Vector Vector::divideByScalar(double scalar) const {
+    return Vector(elements[0] / scalar, elements[1] / scalar, elements[2] / scalar);
+}
+
+Vector Vector::dividingScalar(double scalar) const {
+    return Vector(scalar / elements[0], scalar / elements[1], scalar / elements[2]);
+}
+
+void Vector::print() const {
+    for (const auto& elem : elements) {
+        std::cout << elem << " ";
     }
+    std::cout << std::endl;
+}
 
-    const double& operator[](int index) const {
-        return elements[index];
-    }
 
-    //Override the + operator for the Vector class
-    Vector operator+(const Vector& other) const {
-        return Vector(elements[0] + other[0], elements[1] + other[1], elements[2] + other[2]);
-    }
-
-    //Override the - operator for the Vector class
-    Vector operator-(const Vector& other) const {
-        return Vector(elements[0] - other[0], elements[1] - other[1], elements[2] - other[2]);
-    }
-
-    double norm() {
-        return sqrt(elements[0] * elements[0] + elements[1] * elements[1] + elements[2] * elements[2]);
-    }
-
-    Vector scalarMultiply(double scalar) const {
-        return Vector(elements[0] * scalar, elements[1] * scalar, elements[2] * scalar);
-    }
-
-    Vector vectorMultiply(const Vector& other) const {
-        return Vector(elements[0] * other[0], elements[1] * other[1], elements[2] * other[2]);
-    }
-
-    Vector power(double p) const {
-        return Vector(pow(elements[0], p), pow(elements[1], p), pow(elements[2], p));
-    }
-
-    Vector divideByScalar(double scalar) const {
-        return Vector(elements[0] / scalar, elements[1] / scalar, elements[2] / scalar);
-    }
-
-    Vector dividingScalar(double scalar) const {
-        return Vector(scalar / elements[0], scalar / elements[1], scalar / elements[2]);
-    }
-
-    void print() const {
-        for (const auto& elem : elements) {
-            std::cout << elem << " ";
-        }
-        std::cout << std::endl;
-    }
-};
+// };
 
---
+++ CPlusPlus/Vector.h
@@ -0,0 +1,33 @@
+#ifndef VECTOR_H
+#define VECTOR_H
+
+#include <array>
+
+class Vector {
+private:
+    std::array<double, 3> elements;
+
+public:
+    Vector();
+    Vector(double x, double y, double z);
+
+    double& operator[](int index);
+    const double& operator[](int index) const;
+
+    Vector operator+(const Vector& other) const;
+    Vector operator-(const Vector& other) const;
+
+    double norm() const;
+
+    Vector scalarMultiply(double scalar) const;
+    Vector vectorMultiply(const Vector& other) const;
+
+    Vector power(double p) const;
+
+    Vector divideByScalar(double scalar) const;
+    Vector dividingScalar(double scalar) const;
+
+    void print() const;
+};
+
+#endif
