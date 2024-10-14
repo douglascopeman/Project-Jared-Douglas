34662c3e2e24b2b5302c3fc7991b19dd7d807a2d
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-14 15:56:58 +0100

    testing cpp vs python output for debugging

--- Python/run.py
+++ Python/run.py
@@ -6,10 +6,10 @@ from orbit_examples import orbit_examples
 import Integrators
 import time
 
-N = 10000 #rename to N
+N = 2500 #rename to N
 dt = 0.01
 
-# simulation = Simulation.Simulation(N, dt, orbit_examples.elliptical, Integrator=Integrators.symplecticEuler)
+simulation = Simulation.Simulation(N, dt, orbit_examples.circular)
 
 # start = time.time()
 # simulation.runFast()
@@ -17,19 +17,26 @@ dt = 0.01
 
 # print("Elapsed Time:" + str(end - start))
 
-# plotter = Plotter.Plotter("Python\Outputs", 
-#                           runFast=False, 
-#                           plot_centre_of_mass=True, 
-#                           plot_energy=False, 
-#                           plot_energy_error=True, 
-#                           animate_orbits=False, 
-#                           plot_angular_momentum_error = True, 
-#                           plot_linear_momentum_error=False, 
-#                           plot_3D=False)
-# plotter.plot()
+plotter = Plotter.Plotter("Python\Outputs", 
+                          runFast=True, 
+                          plot_centre_of_mass=False, 
+                          plot_energy=False, 
+                          plot_energy_error=False, 
+                          animate_orbits=False, 
+                          plot_angular_momentum_error = False, 
+                          plot_linear_momentum_error=False, 
+                          plot_3D=False)
 
-plotter = Plotter.Plotter("CPlusPlus\Outputs", runFast=True, plot_centre_of_mass=False, plot_energy=False, plot_energy_error=False, animate_orbits=False, plot_angular_momentum_error=False, plot_3D=False)
 plotter.plot()
 
-# plotter = Plotter.Plotter("JavaSimulation\\Outputs", runFast=False, plot_centre_of_mass=False, plot_energy=False, plot_energy_error=False, animate_orbits=False, plot_angular_momentum_error=False, plot_3D=False)
-# plotter.plot()
+plotter_c = Plotter.Plotter("CPlusPlus\Outputs", 
+                          runFast=True, 
+                          plot_centre_of_mass=False, 
+                          plot_energy=False, 
+                          plot_energy_error=False, 
+                          animate_orbits=False, 
+                          plot_angular_momentum_error = False, 
+                          plot_linear_momentum_error=False, 
+                          plot_3D=False)
+plotter_c.plot()
+
