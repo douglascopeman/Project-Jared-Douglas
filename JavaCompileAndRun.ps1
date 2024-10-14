f058af05dbd8d050b56f672da9c4dc95c9f14502
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-14 17:02:44 +0100

    updated compileandrun scripts

--- JavaCompileAndRun.ps1
+++ JavaCompileAndRun.ps1
@@ -1,5 +1,7 @@
 param ($N = $(throw "N parameter is required."), $dt = $(throw "dt parameter is required."))
 
-javac .\JavaSimulation\*.java
+Set-Location .\JavaSimulation
+javac .\*.java
+Set-Location ..
 java javasimulation/Run $N $dt
 write-host "Done!"
