11fb4b243ee67158feaf4c567db36e116ec83e52
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-14 18:19:05 +0100

    added command line parameter reading for julia

--- Julia/Run.jl
+++ Julia/Run.jl
@@ -6,12 +6,18 @@ include("Body.jl")
 using .Simulation
 
 # Defining the model space
-moon = ModelSpace.Body(position = [1,0,0], velocity =[0,0.6,0])
-earth = ModelSpace.Body(position = [-1,0,0], velocity = [0,-0.6,0])
+moon = ModelSpace.Body(position = [1,0,0], velocity =[0,0.5,0])
+earth = ModelSpace.Body(position = [-1,0,0], velocity = [0,-0.5,0])
 
 bodies = [moon, earth]
 simLength = 10000
-dt = 0.01    
+dt = 0.01  
+
+if length(ARGS) > 0
+    print("Arguments: ", ARGS)
+    simLength = parse(Int64, ARGS[1])
+    dt = parse(Float64, ARGS[2])
+end
 
 @btime Simulation.runSimulation(bodies, simLength, dt)
 
