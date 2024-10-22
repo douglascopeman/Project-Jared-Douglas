4849b9aaa4a8ce229f07419b87a9c46aa0d9446d
douglascopeman <s2147499@ed.ac.uk>
2024-10-22 17:15:09 +0100

    Fixed variable dt error from meeting

--- Python/Testing.py
+++ Python/Testing.py
@@ -3,23 +3,28 @@ import Plotter
 import Simulation
 from orbit_examples import orbit_examples
 import Integrators
+import time
 
 N = 5000 #rename to N
-dt = 0.05
+dt = 0.005
 
-simulation = Simulation.Simulation(N, 
+simulation = Simulation.Simulation(N,
                                    dt,
-                                   orbit_examples.figure_eight,
-                                   Integrator = Integrators.yoshida,
-                                   is_focus_on_body = False
-                                #    stop_conditions = {
-                                #        "energy_error_bound": 1,
-                                #         "distance_bound": 10,
-                                #         "variable_dt_bound": 1*10**(-8)
-                                #    }
+                                   orbit_examples.elliptical_difficult,
+                                   Integrator = Integrators.three_step_leapfrog,
+                                   is_focus_on_body = False,
+                                   is_variable_dt = True,
+                                 #   stop_conditions = {
+                                 #       "energy_error_bound": 0.001,
+                                 #        "distance_bound": 20,
+                                 #        "variable_dt_bound": 1*10**(-6)
+                                 #   }
                                    )
 
+start = time.time()
 simulation.run()
+print(time.time() - start)
+
 
 plotter = Plotter.Plotter("Python\\Outputs", 
                           run_fast=False, 
