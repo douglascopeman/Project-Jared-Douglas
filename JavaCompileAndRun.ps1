3ee2a78b219b0213ff54dc6a77281dfa6ac95812
Jared Bruggink <s2162590@ed.ac.uk>
2024-10-29 12:35:54 +0000

    changed extra parameters to be passed as a list

--- JavaCompileAndRun.ps1
+++ JavaCompileAndRun.ps1
@@ -1,9 +1,25 @@
-param ($N = $(throw "N parameter is required for Cpp."), $dt = $(throw "dt parameter is required for Cpp."))
+param ([Parameter(
+                Mandatory=$True,
+                Position = 0
+                )
+        ][int] $N = $(throw "N parameter is required for Java."), 
+        [Parameter(
+                Mandatory=$True,
+                Position = 1
+                )
+        ][double] $dt = $(throw "dt parameter is required for Java."),
+        [Parameter(
+                Mandatory=$False,
+                ValueFromRemainingArguments=$true,
+                Position = 2
+                )
+        ][string[]] $listArgs
+        )
 
 Set-Location .\javasimulation
 javac .\*.java -d .\
 Copy-Item -Path .\Outputs\* -Destination .\javasimulation\Outputs -Recurse -Force
-java javasimulation/Run $N $dt
+java javasimulation/Run $N $dt $listArgs
 Copy-Item -Path .\javasimulation\Outputs\* -Destination .\Outputs -Recurse -Force
 Set-Location ..
 write-host "Done!"
