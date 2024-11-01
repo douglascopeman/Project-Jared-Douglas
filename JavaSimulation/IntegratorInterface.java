01161c1452e84a2543bf5ed1d070822a7b13346b
Jared Bruggink <s2162590@ed.ac.uk>
2024-11-01 13:16:53 +0000

    Removed integrator interface and deprecated integrator type

--- JavaSimulation/IntegratorInterface.java
+++
@@ -1,5 +0,0 @@
-package javasimulation;
-
-public interface IntegratorInterface{
-    Body[] Integrate(Body[] bodies, double dt);
-}
--- JavaSimulation/IntegratorType.java
+++ JavaSimulation/IntegratorType.java
@@ -1,6 +1,7 @@
 package javasimulation;
 
 // Define IntegratorType enum to represent different integration methods
+@Deprecated
 public enum IntegratorType {
     EULER,
     SYMPLECTIC_EULER,
