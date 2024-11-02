86880f9d01a94eedc53e033b7f28e9324e41ef9b
Jared Bruggink <s2162590@ed.ac.uk>
2024-11-02 13:25:00 +0000

    changed to take flags for boolean and other optional variables

--- JavaCompileAndRun.ps1
+++ JavaCompileAndRun.ps1
@@ -2,24 +2,70 @@ param ([Parameter(
                 Mandatory=$True,
                 Position = 0
                 )
-        ][int] $N = $(throw "N parameter is required for Java."), 
+        ]
+        [string] $Orbit = $(throw "Orbit parameter is required for Java."), 
         [Parameter(
                 Mandatory=$True,
                 Position = 1
                 )
-        ][double] $dt = $(throw "dt parameter is required for Java."),
+        ]
+        [string] $N = $(throw "N parameter is required for Java."),
         [Parameter(
-                Mandatory=$False,
-                ValueFromRemainingArguments=$true,
+                Mandatory=$True,
                 Position = 2
                 )
-        ][string[]] $listArgs
-        )
+        ]
+        [string] $dt = $(throw "dt parameter is required for Java."),
+
+        [Parameter(Mandatory=$False)]
+        [string] $integrator = "symplecticEuler",
+        [Parameter(Mandatory=$False)]
+        [switch] $useVariableTimestep,
+        [Parameter(Mandatory=$False)]
+        [switch] $checkStopConditions,
+        [Parameter(Mandatory=$False)]
+        [switch] $calculateCenterOfMass,
+        [Parameter(Mandatory=$False)]
+        [switch] $calculateEnergies,
+        [Parameter(Mandatory=$False)]
+        [switch] $calculateAngularMomentum,
+        [Parameter(Mandatory=$False)]
+        [switch] $calculateLinearMomentum,
+        [Parameter(Mandatory=$False)]
+        [switch] $findOrbitLength
+)
+
+$javaArgs = @($Orbit, $N, $dt)
+if ($integrator){
+        $javaArgs += "-integrator " + $integrator
+}
+
+if ($useVariableTimestep) {
+        $javaArgs += "-useVariableTimestep"
+}
+if ($checkStopConditions) {
+        $javaArgs += "-checkStopConditions"
+}
+if ($calculateCenterOfMass) {
+        $javaArgs += "-calculateCenterOfMass"
+}
+if ($calculateEnergies) {
+        $javaArgs += "-calculateEnergies"
+}
+if ($calculateAngularMomentum) {
+        $javaArgs += "-calculateAngularMomentum"
+}
+if ($calculateLinearMomentum) {
+        $javaArgs += "-calculateLinearMomentum"
+}
+if ($findOrbitLength) {
+        $javaArgs += "-findOrbitLength"
+}
 
 Set-Location .\javasimulation
 javac .\*.java -d .\
 Copy-Item -Path .\Outputs\* -Destination .\javasimulation\Outputs -Recurse -Force
-java javasimulation/Run $N $dt $listArgs
+java javasimulation/Run $javaArgs
 Copy-Item -Path .\javasimulation\Outputs\* -Destination .\Outputs -Recurse -Force
 Set-Location ..
 write-host "Done!"
