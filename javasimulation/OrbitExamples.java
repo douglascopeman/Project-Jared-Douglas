ab93ad18e2b4a4e06f1b1fbccdebda5fab835010
Jared Bruggink <s2162590@ed.ac.uk>
2024-11-02 13:26:07 +0000

    added hashmap to look up orbit examples

--- javasimulation/OrbitExamples.java
+++ javasimulation/OrbitExamples.java
@@ -1,6 +1,10 @@
 package javasimulation;
 
+import java.util.HashMap;
+
 public class OrbitExamples {
+
+    // Remember to ammend HashMap at the end of the file when adding new examples
 
     public static Body[] circular = {
         new Body(new Vector(1, 0, 0), new Vector(0, 0.5, 0)), 
@@ -28,4 +32,12 @@ public class OrbitExamples {
         new Body(new Vector(-0.97000436,0.24308753,0), new Vector(0.46620368,0.43236573,10)),
         new Body(new Vector(), new Vector(-0.93240737,-0.86473146,10))
     };
+
+    public static HashMap<String, Body[]> orbitExampleMap = new HashMap<String, Body[]>() {{
+        put("circular", OrbitExamples.circular);
+        put("elliptical", elliptical);
+        put("longElliptical", longElliptical);
+        put("figureEight", figureEight);
+        put("figureEightMoving", figureEightMoving);
+    }};
 }
