b77ac0c1a14b28efed0c721825a9c3bbd9e5ba9b
Jared Bruggink <s2162590@ed.ac.uk>
2024-11-03 14:52:44 +0000

    added temporary override for testing perturbations

--- javasimulation/Run.java
+++ javasimulation/Run.java
@@ -1,8 +1,24 @@
 package javasimulation;
+import java.util.Scanner;
 
 public class Run{
 
     public static void main(String[] args) {
+
+        System.out.println("Temp override for testing perturbations (true/false): ");
+        Scanner scanner = new Scanner(System.in);
+        boolean tempOverride = scanner.nextBoolean();
+
+        if (tempOverride) {
+            RunPerturbations runPerturbations = new RunPerturbations(OrbitExamples.orbitExampleMap.get("figureEight"), 1000, 0.01, 1, 0.01);
+            double[][] stopMatrix = runPerturbations.run();
+            runPerturbations.saveStopMatrix(stopMatrix);
+            scanner.close();
+            return;
+        }
+        scanner.close();
+
+
 
         Body[] bodies = OrbitExamples.orbitExampleMap.get(args[0]);
         int N = Integer.parseInt(args[1]);
