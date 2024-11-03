d3b0e077728457ca196c31f47bc06baee359749c
douglascopeman <s2147499@ed.ac.uk>
2024-11-03 16:17:57 +0000

    Pertubation tests

--- Python/Testing.py
+++ Python/Testing.py
@@ -4,10 +4,11 @@ import Simulation
 from orbit_examples import orbit_examples
 import Integrators
 import Perturbation
+import PertubationPlot
 import time
 
-N = 50000 #rename to N
-dt = 0.01
+N = 10000 #rename to N
+dt = 0.1
 
 # simulation = Simulation.Simulation(N,
 #                                    dt,
@@ -29,29 +30,32 @@ dt = 0.01
 #   simulation.run_fast()
 # print(time.time() - start)
 
-pertubation = Perturbation.Pertubation(
-  N, dt, orbit_examples.figure_eight, 8, 0.01,
-                                     stop_conditions = {
-                                       "energy_error_bound": 0.001,
-                                        "distance_bound": 20,
-                                        "variable_dt_bound": 1*10**(-6)
-                                   }
-)
-pertubation.run()
+# pertubation = Perturbation.Pertubation(
+#   N, dt, orbit_examples.figure_eight, 4, 0.01,
+#                                      stop_conditions = {
+#                                        "energy_error_bound": 0.001,
+#                                         "distance_bound": 20,
+#                                         "variable_dt_bound": 1*10**(-6)
+#                                    }
+# )
+# pertubation.run()
 
 
 
 
-plotter = Plotter.Plotter("Python\\Outputs", 
-                          run_fast=False, 
-                          plot_centre_of_mass=True, 
-                          plot_energy=False, 
-                          plot_energy_error=True, 
-                          plot_angular_momentum_error = True, 
-                          plot_linear_momentum_error=False, 
-                          plot_3D=False,
-                          x_label="Time",
-                          save_plots=True
-                          )
+# plotter = Plotter.Plotter("Python\\Outputs", 
+#                           run_fast=False, 
+#                           plot_centre_of_mass=True, 
+#                           plot_energy=False, 
+#                           plot_energy_error=True, 
+#                           plot_angular_momentum_error = True, 
+#                           plot_linear_momentum_error=False, 
+#                           plot_3D=False,
+#                           x_label="Time",
+#                           save_plots=True
+#                           )
 
-plotter.plot()
+# plotter.plot()
+
+pertubation_plot = PertubationPlot.PertubationPlot("Python\\Outputs")
+pertubation_plot.plot()
