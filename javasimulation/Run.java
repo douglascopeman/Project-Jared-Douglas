a5ac6424d2ebe9ef13c11a2e48788fb5cbee71a0
Jared Bruggink <s2162590@ed.ac.uk>
2024-11-04 14:32:48 +0000

    neated up run code; Still needs improvements

--- javasimulation/Run.java
+++ javasimulation/Run.java
@@ -1,35 +1,44 @@
 package javasimulation;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.List;
 
 public class Run{
 
     public static void main(String[] args) {
 
-        //#region testing for perturbations
-
-        System.out.println("Temp override for testing perturbations (true/false): ");
-        Scanner scanner = new Scanner(System.in);
-        boolean tempOverride = scanner.nextBoolean();
-
-        if (tempOverride) {
-            OutputWriter.setupDirectories();
-            Perturbations runPerturbations = new Perturbations(OrbitExamples.orbitExampleMap.get("figureEight"), 10000, 0.01, 201, 0.001);
-            double[][] stopMatrix = runPerturbations.run();
-            OutputWriter.saveMatrix(stopMatrix);
-            scanner.close();
-            return;
-        }
-        scanner.close();
-
-        //#endregion
-
         Body[] bodies = OrbitExamples.orbitExampleMap.get(args[0]);
         int N = Integer.parseInt(args[1]);
         double dt = Double.parseDouble(args[2]);
         String[] commandlineOptions = java.util.Arrays.copyOfRange(args, 3, args.length);
+        List<String> clOptionsList = java.util.Arrays.asList(commandlineOptions);
 
-        Simulation simulation = new Simulation(bodies, N, dt, commandlineOptions);
-        simulation.run();
+        if (clOptionsList.contains("-perturbate")) {
+            // This definitely needs to be refactored
+            int halfGridSize = clOptionsList.stream()
+                                            .filter(s -> s.startsWith("-halfGridSize "))
+                                            .map(s -> s.split(" ")[1])
+                                            .mapToInt(Integer::parseInt)
+                                            .findFirst()
+                                            .orElseThrow(() -> new IllegalArgumentException("Missing -halfGridSize option"));
+            double delta = clOptionsList.stream()
+                                        .filter(s -> s.startsWith("-delta "))
+                                        .map(s -> s.split(" ")[1])
+                                        .mapToDouble(Double::parseDouble)
+                                        .findFirst()
+                                        .orElseThrow(() -> new IllegalArgumentException("Missing -delta option"));
+
+            System.out.println("Perturbating with halfGridSize: " + halfGridSize + " and delta: " + delta);
+            OutputWriter.setupDirectories();
+            Perturbations runPerturbations = new Perturbations(bodies, N, dt, halfGridSize, delta);
+            double[][] stopMatrix = runPerturbations.run();
+            OutputWriter.saveMatrix(stopMatrix);
+        } else {
+
+            Simulation simulation = new Simulation(bodies, N, dt, commandlineOptions);
+            simulation.run();
+
+        }
 
     }
 }
