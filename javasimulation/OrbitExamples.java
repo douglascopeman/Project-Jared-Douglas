e2211782c493bf662dea5bc290b3aba96f6b2f2e
douglascopeman <s2147499@ed.ac.uk>
2024-11-19 13:26:34 +0000

    Began to include some different figure 8 orbits for different "energy layers"

--- javasimulation/OrbitExamples.java
+++ javasimulation/OrbitExamples.java
@@ -33,11 +33,42 @@ public class OrbitExamples {
         new Body(new Vector(0.97000436,-0.24308753,0), new Vector(0.46620368,0.43236573,10))
     };
 
+
+
+
+    // ============== Figure 8 orbits with different energy ========================
+    public static Body[] figureEightEnergy10PercentMore = {
+        new Body(new Vector(0.97000436,-0.24308753,0), new Vector(0.4903179,0.45472969,0)),
+        new Body(new Vector(), new Vector(-0.98063579,-0.90945938,0)),
+        new Body(new Vector(-0.97000436,0.24308753,0), new Vector(0.4903179 ,0.45472969,0))
+    };
+    public static Body[] figureEightEnergy10PercentLess = {
+        new Body(new Vector(0.97000436,-0.24308753,0), new Vector(0.44077217,0.40878009,0)),
+        new Body(new Vector(), new Vector(-0.88154434,-0.81756017,0)),
+        new Body(new Vector(-0.97000436,0.24308753,0), new Vector(0.44077217 ,0.40878009,0))
+    };
+    public static Body[] figureEightEnergy20PercentLess = {
+        new Body(new Vector(0.97000436,-0.24308753,0), new Vector(0.41378054,0.38374757,0)),
+        new Body(new Vector(), new Vector(-0.82756109,-0.76749513,0)),
+        new Body(new Vector(-0.97000436,0.24308753,0), new Vector(0.41378054 ,0.38374757,0))
+    };
+    public static Body[] figureEightEnergy20PercentMore = {
+        new Body(new Vector(0.97000436,-0.24308753,0), new Vector(0.5133005,0.47604418,0)),
+        new Body(new Vector(), new Vector(-1.02660101,-0.95208835,0)),
+        new Body(new Vector(-0.97000436,0.24308753,0), new Vector(0.5133005 ,0.47604418,0))
+    };
+
+
+
     public static HashMap<String, Body[]> orbitExampleMap = new HashMap<String, Body[]>() {{
         put("circular", OrbitExamples.circular);
         put("elliptical", elliptical);
         put("longElliptical", longElliptical);
         put("figureEight", figureEight);
         put("figureEightMoving", figureEightMoving);
+        put("figureEightEnergy10PercentMore", figureEightEnergy10PercentMore);
+        put("figureEightEnergy10PercentLess", figureEightEnergy10PercentLess);
+        put("figureEightEnergy20PercentMore", figureEightEnergy20PercentMore);
+        put("figureEightEnergy20PercentLess", figureEightEnergy20PercentLess);
     }};
 }
