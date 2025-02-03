61b1fe9cb4297beec2f62830bee404b2c4343773
douglascopeman <s2147499@ed.ac.uk>
2025-02-03 12:42:59 +0000

    Changed Run CPP to use elliptical orbit

--- Compare/CPlusPlus/Run.cpp
+++ Compare/CPlusPlus/Run.cpp
@@ -15,8 +15,8 @@ int main(int argc, char* argv[]) {
     double dt = std::stod(argv[2]);
 
     // Instantiate two Body objects
-    Body body1(Vector(1, 0, 0), Vector(0, 0.5, 0), 1);
-    Body body2(Vector(-1, 0, 0), Vector(0, -0.5, 0), 1);
+    Body body1(Vector(1, 0, 0), Vector(0, 0.6, 0), 1);
+    Body body2(Vector(-1, 0, 0), Vector(0, -0.6, 0), 1);
     std::vector<Body*> bodies = {&body1, &body2}; // Vector of body pointers
 
     Simulation simulation(bodies, N, dt);
