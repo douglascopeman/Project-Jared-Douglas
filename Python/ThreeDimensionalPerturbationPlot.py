9bb78cc5154539379e790ea679acc10a637e1ab8
douglascopeman <s2147499@ed.ac.uk>
2025-02-06 15:11:56 +0000

    updated animation plotter to accomadate energy perturbation folders

--- Python/ThreeDimensionalPerturbationPlot.py
+++ Python/ThreeDimensionalPerturbationPlot.py
@@ -16,7 +16,7 @@ class ThreeDimensionalPerturbationPlot():
     def __init__(self, output_directory):
         self.output_directory = output_directory
 
-    def read_data(self):
+    def read_data(self, is_energy):
 
         # Reading the settings file
         with open(os.path.join(self.output_directory, "3dperturbationSettings.csv"), 'r') as f:
@@ -27,28 +27,44 @@ class ThreeDimensionalPerturbationPlot():
             self.p_axis1 = int(data[3])
             self.p_axis2 = int(data[4])
             self.plot_size_axis1 = 2*self.p_axis1 + 1
+            if is_energy:
+                self.plot_size_axis2 = 2*self.p_axis2 +1
+            else:
+                self.plot_size_axis2 = self.p_axis2
             # Axis 1 will be the dimension of the matrix in each csv file and axis 2
             # will be the number of csv files
 
             # Initialising the matrix variables
-            self.time_matrix = np.zeros((self.plot_size_axis1, self.plot_size_axis1, self.p_axis2), dtype=int)
-            self.stop_code_matrix = np.zeros((self.plot_size_axis1, self.plot_size_axis1, self.p_axis2), dtype=str)
+            self.time_matrix = np.zeros((self.plot_size_axis1, self.plot_size_axis1, self.plot_size_axis2), dtype=int)
+            self.stop_code_matrix = np.zeros((self.plot_size_axis1, self.plot_size_axis1, self.plot_size_axis2), dtype=str)
+        
+        if is_energy:
+            for i in range(-self.p_axis2, self.p_axis2):
+                # Reading the timeMatrix
+                with open(os.path.join(self.output_directory, "timeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
+                    data = np.loadtxt(f, delimiter=",", dtype=int)
 
-        for i in range(self.p_axis2):
-             # Reading the timeMatrix
-            with open(os.path.join(self.output_directory, "timeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
-                data = np.loadtxt(f, delimiter=",", dtype=int)
-                self.time_matrix[:,:,i] = data
-            # Reading the stopCodeMatrix
-            with open(os.path.join(self.output_directory, "stopCodeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
-                data = np.loadtxt(f, delimiter=",", dtype=str)
-                self.stop_code_matrix[:,:,i] = data
+                    self.time_matrix[:,:,i+self.p_axis2-1] = data
+                # Reading the stopCodeMatrix
+                with open(os.path.join(self.output_directory, "stopCodeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
+                    data = np.loadtxt(f, delimiter=",", dtype=str)
+                    self.stop_code_matrix[:,:,i+self.p_axis2-1] = data
+        else:
+            for i in range(self.p_axis2):
+                # Reading the timeMatrix
+                with open(os.path.join(self.output_directory, "timeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
+                    data = np.loadtxt(f, delimiter=",", dtype=int)
+                    self.time_matrix[:,:,i] = data
+                # Reading the stopCodeMatrix
+                with open(os.path.join(self.output_directory, "stopCodeMatrix" + str(i*self.delta_axis2) + ".csv"), 'r') as f:
+                    data = np.loadtxt(f, delimiter=",", dtype=str)
+                    self.stop_code_matrix[:,:,i] = data
 
-    def animate(self):
+    def animate(self, is_energy=False):
         '''
         Creates an animation with each frame a different energy/angular momentum shift
         '''
-        self.read_data()
+        self.read_data(is_energy)
 
         fig, ax = plt.subplots(figsize=(10, 8))
 
@@ -57,17 +73,18 @@ class ThreeDimensionalPerturbationPlot():
 
         skip_no_labels = np.size(axis_labels)//10  # To declutter the axis labeling we only show 10 labels
 
-        heatmap = sns.heatmap(self.time_matrix[:,:,0].T, cbar=True,xticklabels=skip_no_labels, yticklabels=skip_no_labels, ax=ax, vmin=self.time_matrix.min(), vmax=self.time_matrix.max(),  norm=LogNorm())
+
+        sns.heatmap(self.time_matrix[:,:,0].T, xticklabels=skip_no_labels, yticklabels=skip_no_labels, ax=ax,  norm=LogNorm(vmin=self.time_matrix.min()+1, vmax=self.time_matrix.max()))
         
         def update(frame):
             """Update function for animation."""
             ax.clear()
             df = pd.DataFrame(self.time_matrix[:, :, frame].T, columns=axis_labels, index=-axis_labels)
-            sns.heatmap(df, cbar=False, xticklabels=skip_no_labels, yticklabels=skip_no_labels, ax=ax, vmin=self.time_matrix.min(), vmax=self.time_matrix.max(), norm=LogNorm())
+            sns.heatmap(df, cbar=False, xticklabels=skip_no_labels, yticklabels=skip_no_labels, ax=ax, norm=LogNorm(vmin=self.time_matrix.min()+1, vmax=self.time_matrix.max()))
     
 
         # Create the animation
-        ani = animation.FuncAnimation(fig, update, frames=self.p_axis2, interval=40)
+        ani = animation.FuncAnimation(fig, update, frames=self.plot_size_axis2, interval=40)
         writervideo = animation.FFMpegWriter(fps=30) 
         # Save the animation to a file or display it
         ani.save('heatmap_animation.mp4', writer=writervideo)
--- Python/plot_perturbation.py
+++ Python/plot_perturbation.py
@@ -2,18 +2,18 @@ import Perturbation_plotter
 import ThreeDimensionalPerturbationPlot
 
 
-perturbation_plot = Perturbation_plotter.Perturbation_plotter("Python\\Outputs")
+# perturbation_plot = Perturbation_plotter.Perturbation_plotter("Python\\Outputs")
 
 #perturbation_plot.plot_time("timeMatrix")
 #perturbation_plot.plot_stop_codes("stopCodeMatrix")
 # perturbation_plot.count_stop_matrix("stopCodeMatrix")
-perturbation_plot.plot_stability("stabilityMatrix", save=False, save_dbl_click=False)
+# perturbation_plot.plot_stability("stabilityMatrix", save=False, save_dbl_click=False)
 
 # perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix")
 # perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix", "stabilityMatrix")
 
 
-# pertubation_3dplot = ThreeDimensionalPerturbationPlot.ThreeDimensionalPerturbationPlot("C:\\Users\\Douglas\\Downloads\\Outputs")
-# pertubation_3dplot.animate()
+pertubation_3dplot = ThreeDimensionalPerturbationPlot.ThreeDimensionalPerturbationPlot("C:\\Users\\Douglas\\Downloads\\Outputs")
+pertubation_3dplot.animate(is_energy=True)
 
 print("Done!")
