5530b7150f1ddcb488f16c8c6c6a6c7c2eba8d65
Jared Bruggink <s2162590@ed.ac.uk>
2025-02-10 17:30:24 +0000

    changed to plot gradient plot

--- Python/plot_perturbation.py
+++ Python/plot_perturbation.py
@@ -7,10 +7,10 @@ perturbation_plot = Perturbation_plotter.Perturbation_plotter("Python\\Outputs")
 #perturbation_plot.plot_time("timeMatrix")
 #perturbation_plot.plot_stop_codes("stopCodeMatrix")
 # perturbation_plot.count_stop_matrix("stopCodeMatrix")
-perturbation_plot.plot_stability("stabilityMatrix", save=False, save_dbl_click=False)
+# perturbation_plot.plot_stability("stabilityMatrix", save=False, save_dbl_click=False)
 
 # perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix")
-# perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix", "stabilityMatrix")
+perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix", "stabilityMatrix")
 
 
 # pertubation_3dplot = ThreeDimensionalPerturbationPlot.ThreeDimensionalPerturbationPlot("C:\\Users\\Douglas\\Downloads\\Outputs")
