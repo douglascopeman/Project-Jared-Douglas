43955a9a91122c75d5a050524874309f8e16e7c6
Jared Bruggink <s2162590@ed.ac.uk>
2025-02-20 18:52:46 +0000

    renamed euler velocity constant

--- javasimulation/OrbitExamples.java
+++ javasimulation/OrbitExamples.java
@@ -21,11 +21,11 @@ public class OrbitExamples {
         new Body(new Vector(-1, 0, 0), new Vector(0, -0.1, 0))
     };
 
-    private static double a = Math.sqrt(5.0/4.0);
+    private static double v = Math.sqrt(5.0/4.0);
     public static Body[] eulerThree = {
-        new Body(new Vector(1, 0, 0), new Vector(0, a, 0)),
+        new Body(new Vector(1, 0, 0), new Vector(0, v, 0)),
         new Body(new Vector(), new Vector()),
-        new Body(new Vector(-1, 0, 0), new Vector(0, -a, 0))
+        new Body(new Vector(-1, 0, 0), new Vector(0, -v, 0))
     };
 
 
