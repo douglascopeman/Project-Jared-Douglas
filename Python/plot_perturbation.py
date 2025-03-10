a75bb354865f5310474ba8ac354a3eae391611a6
Jared Bruggink <s2162590@ed.ac.uk>
2025-03-10 20:50:44 +0000

    added all error codes

--- Python/plot_perturbation.py
+++ Python/plot_perturbation.py
@@ -2,7 +2,7 @@ import Perturbation_plotter
 import ThreeDimensionalPerturbationPlot
 
 
-perturbation_plot = Perturbation_plotter.Perturbation_plotter("../Project-Jared-Douglas-OneDrive/testing_perturbations/deltaTmin_e-5_test")
+perturbation_plot = Perturbation_plotter.Perturbation_plotter("../Project-Jared-Douglas-OneDrive/testing_perturbations/deltaTmin_e-10_test")
 #perturbation_plot = Perturbation_plotter.Perturbation_plotter("testing_perturbations")
 
 #perturbation_plot.plot_time("timeMatrix")
@@ -10,7 +10,7 @@ perturbation_plot = Perturbation_plotter.Perturbation_plotter("../Project-Jared-
 # perturbation_plot.count_stop_matrix("stopCodeMatrix")
 # perturbation_plot.plot_stability("stabilityMatrix", save=False, save_dbl_click=True)
 
-perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix", save=True, categories_= ['D','V','F','X'])
+perturbation_plot.plot_stop_codes_gradient("timeMatrix", "stopCodeMatrix", save=False, categories_= ['D','V','E','F','X'])
 #perturbation_plot.plot_stop_codes_stab_gradient("timeMatrix", "stopCodeMatrix", "stabilityMatrix")
 
 
